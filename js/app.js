c45b123e7505404cd25f36db0b71fbc575f5b1d2
DreamerAEKer <dreameraeker@gmail.com>
2026-02-27 00:38:09 +0700

    update files

--- js/app.js
+++ js/app.js
@@ -825,7 +825,7 @@ const App = {
         await App.alert(`โหลดบิล ${billId} เรียบร้อย\nแก้ไขรายการแล้วกด "ชำระเงิน" เพื่อบันทึกทับบิลเดิม`);
     },
 
-    VERSION: '0.89.27', // Update Version
+    VERSION: '0.89.28', // Update Version
 
     // --- Settings View ---
     renderSettingsView: (container) => {
